c47bc330d25db37ba73c7c16fbfe7e8ab8355acd
KT <tran.nk7@gmail.com>
2020-09-30 17:39:32 -0700

    2020.09.30

--- 1.prep.R
+++ 1.prep.R
@@ -26,7 +26,36 @@ countyNYT %<>%
 
   replace_na(list(newCases = 0
                 , casesTminus1 = 0
-                , casesTminus2 = 0)) 
+                , casesTminus2 = 0)) %>%
+
+
+  # ADD HOLIDAY
+  mutate(anomalousWeekend = 
+         case_when(
+           date %in% as.Date(c('2020-03-12', '2020-03-13', '2020-03-14', '2020-03-15'
+           )) ~ 1
+           , TRUE ~ 0
+         )
+       
+       , longWeekend = 
+         case_when(
+          date %in% as.Date(c(
+             '2020-02-15', '2020-02-16', '2020-02-17'
+             , '2020-04-10', '2020-04-11', '2020-04-12', '2020-04-13'
+             , '2020-05-23', '2020-05-24', '2020-05-25'
+             , '2020-07-03', '2020-07-04', '2020-07-05'
+             , '2020-09-04', '2020-09-05', '2020-09-06', '2020-09-07'
+           ))
+           ~ 1
+          , TRUE ~ 0
+         )
+      
+       , weekend = case_when(
+         lubridate::wday(date, label = TRUE) %in% c('Sat', 'Sun') ~ 1
+         , TRUE ~ 0
+       )
+)
+
 
 
 write.csv(countyNYT
